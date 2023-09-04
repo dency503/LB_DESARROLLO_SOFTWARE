7b1fab782f7158c1f6a53adfd8f8dbd3cc22ab6c
Dennis <dennisgodinez64@gmail.com>
2023-09-03 21:54:15 -0600

    first commit

---
+++ Notas_G1/src/notas_g1/Notas_G1.java
@@ -0,0 +1,17 @@
+
+package notas_g1;
+
+/**
+ *
+ * @author FaMel
+ */
+public class Notas_G1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
